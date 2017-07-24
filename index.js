52dade3e525e7be4157ab47e55e3d9672fa7f60e
root <root@vps413074.ovh.net>
2017-07-24 04:28:29 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,115 @@
+var express = require('express')
+var app = express();
+var io = require('socket.io')(8080);
+var fs = require('fs')
+const dns = require('dns');
+const uuidv4 = require('uuid/v4');
+const pug = require('pug');
+
+RconConnection = {}
+
+
+var Rcon = require('rcon');
+var options = {
+  tcp: false,       // false for UDP, true for TCP (default true)
+  challenge: false  // true to use the challenge protocol (default true)
+};
+var online = 0;
+
+app.engine('pug', require('pug').__express);
+app.listen(80, function () {
+    console.log('http server started on port 80');
+});
+
+/* client.on('auth', function() {
+  console.log("Authed!");
+
+}).on('response', function(str) {
+  console.log("Got response: " + str);
+
+}).on('end', function() {
+  console.log("Socket closed!");
+}); */
+
+// APP.GET REQUEST
+app.get('/', function(req, res){
+    res.end("403 Forbidden")
+});
+app.get('/rcon', function (req, res) {
+  _tunnel = uuidv4()
+  var data = pug.renderFile('ui.pug', {
+    tunnel: _tunnel
+  });
+
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  res.end(data)
+
+
+/*   client.connect()
+  client.send("restart chat") */
+});
+
+
+io.on('connection', function (socket) {
+  online = online+1
+  console.log("\033[2J Online: " + online);
+  socket.on('rcon_connect', function (data) {
+    patt = new RegExp("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$|^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$|^(?:(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){6})(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:::(?:(?:(?:[0-9a-fA-F]{1,4})):){5})(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})))?::(?:(?:(?:[0-9a-fA-F]{1,4})):){4})(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,1}(?:(?:[0-9a-fA-F]{1,4})))?::(?:(?:(?:[0-9a-fA-F]{1,4})):){3})(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,2}(?:(?:[0-9a-fA-F]{1,4})))?::(?:(?:(?:[0-9a-fA-F]{1,4})):){2})(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,3}(?:(?:[0-9a-fA-F]{1,4})))?::(?:(?:[0-9a-fA-F]{1,4})):)(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,4}(?:(?:[0-9a-fA-F]{1,4})))?::)(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,5}(?:(?:[0-9a-fA-F]{1,4})))?::)(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,6}(?:(?:[0-9a-fA-F]{1,4})))?::))))$")
+    if (patt.test(data.ip) == false || data.port > 65536 || data.port < 1){
+      newclient = null;
+      socket.emit("rcon_response", {content: "Error bad hostname_1"})
+    }else{
+      newclient =  new Rcon(data.ip, data.port, data.password, options);
+      if (newclient != null){
+        newclient.on("auth", function(){
+          socket.emit("rcon_response", {content: "Binded to " + data.ip + ":" + data.port})
+        });
+        newclient.on("response", function(str){
+          if (str.startsWith("rint")){
+            str = str.slice( 1 );
+            str = str.slice( 1 );
+            str = str.slice( 1 );
+            str = str.slice( 1 );
+          }
+          while (str.indexOf("\n") !== -1){
+            str = str.replace("\n", "<br>")
+          }
+          socket.emit("rcon_response", {content: str})
+
+        })
+      }
+
+      if (RconConnection[socket.id] != null){
+        RconConnection[socket.id].disconnect()
+      }
+
+      RconConnection[socket.id] = newclient
+      RconConnection[socket.id].connect();
+      dns.resolve4(data.ip, (err, addresses) => {
+        if (err){
+         // socket.emit("rcon_response", {content: "Error bad hostname-2"})
+        //  RconConnection[socket.id] = null;
+        }else{
+         // RconConnection[socket.id].connect()
+        }
+      });
+    }
+  });
+  socket.on("disconnect", function(){
+    RconConnection[socket.id] = null;
+    online = online - 1
+    console.log("\033[2J Online: " + online);
+  })
+  socket.on("rcon_command", function(data){
+    if (RconConnection[socket.id] != null){
+      RconConnection[socket.id].send(data);
+    }else{
+      socket.emit("rcon_response", {content: "not logged"})
+
+    }
+  })
+});
+String.prototype.startsWith = function(needle)
+{
+    return(this.indexOf(needle) == 0);
+};
---
+++ script.js
@@ -0,0 +1,86 @@
+var errorShow = false;
+var tunnel = $("#tunnel").text();
+var socket = io('http://sheepy.ovh:8080');
+var lastcommand = "";
+
+socket.on('connect', function () {
+    socket.on('rcon_response', function(data){
+        log(data.content)
+    });
+});
+
+
+socket.on('connect_error', function() {
+    if (errorShow == false){
+        alert("Error: no response")
+        errorShow = true
+    }
+});
+
+$("#connectbtn").on("click", function(){
+    endpoint = $("#endpoint").val()
+    pass = $("#password").val()
+
+    remoteInfo = endpoint.split(":")
+    patt = new RegExp("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$|^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$|^(?:(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){6})(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:::(?:(?:(?:[0-9a-fA-F]{1,4})):){5})(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})))?::(?:(?:(?:[0-9a-fA-F]{1,4})):){4})(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,1}(?:(?:[0-9a-fA-F]{1,4})))?::(?:(?:(?:[0-9a-fA-F]{1,4})):){3})(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,2}(?:(?:[0-9a-fA-F]{1,4})))?::(?:(?:(?:[0-9a-fA-F]{1,4})):){2})(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,3}(?:(?:[0-9a-fA-F]{1,4})))?::(?:(?:[0-9a-fA-F]{1,4})):)(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,4}(?:(?:[0-9a-fA-F]{1,4})))?::)(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9]))\.){3}(?:(?:25[0-5]|(?:[1-9]|1[0-9]|2[0-4])?[0-9])))))))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,5}(?:(?:[0-9a-fA-F]{1,4})))?::)(?:(?:[0-9a-fA-F]{1,4})))|(?:(?:(?:(?:(?:(?:[0-9a-fA-F]{1,4})):){0,6}(?:(?:[0-9a-fA-F]{1,4})))?::))))$")
+
+    if (remoteInfo.length == 2 && patt.test(remoteInfo[0])){
+        if(pass == ""){
+            alert("error you need to enter a password")
+        }else{
+            socket.emit("rcon_connect", {ip: remoteInfo[0], port: remoteInfo[1], password: pass})
+        }
+    }else{
+        alert("Bad hostname (example: 127.0.0.1:30120)")
+    }
+});
+
+
+$('#commandInput').bind("enterKey",function(e){
+    var content = $('#commandInput').val()
+    log(content);
+    lastcommand = content
+    if (content != "clear"){
+        socket.emit("rcon_command", content)
+    }else{
+        $("#log").empty();
+    }
+    $('#commandInput').val("")
+
+ });
+
+
+ $('#commandInput').keyup(function(e){
+     if(e.keyCode == 13)
+     {
+         $(this).trigger("enterKey");
+     }else if(e.keyCode == 38){
+         $(this).val(lastcommand)
+     }
+ });
+
+ $('#endpoint').keyup(function(e){
+    if(e.keyCode == 13)
+    {
+        $("#connectbtn").click()
+    }
+});
+
+$('#password').keyup(function(e){
+    if(e.keyCode == 13)
+    {
+        $("#connectbtn").click()
+    }
+});
+
+
+function log(text){
+    time = new Date();
+    $("#log").append("<div class='logRow'><a style='color: rgb(41, 128, 185); text-decoration:none;'>["+
+      ("0" + time.getHours()).slice(-2)   + ":" +
+      ("0" + time.getMinutes()).slice(-2) + ":" +
+      ("0" + time.getSeconds()).slice(-2) + 
+      "] </a><a style='color: black;text-decoration:none;'>" + text + "</a></div>")
+    $("#log").animate({ scrollTop: $(document).height() }, "fast");
+
+}
